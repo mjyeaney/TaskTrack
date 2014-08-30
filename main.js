0f9f958c9545f1ac1445ca63ea02ce2047eb7548
Michael Yeaney <michael.yeaney@gmail.com>
2014-08-30 13:00:00 -0400

    Locked swimlane headers; implemented some basic zooming

--- main.js
+++ main.js
@@ -5,12 +5,28 @@ $(function(){
         $('ol.projects li').width(pos.left + pos.width);
     };
 
-    matchTimelineWidth();
-
     // Synchronize scrolling between project area and timeline
     $('#planChart').on('scroll', function(){
         $('#timeline').scrollLeft($(this).scrollLeft());
+        $('ol.projects li h3').css('margin-left', $(this).scrollLeft()); 
     });
 
-    $('#slider').slider();
+    // Create zoom slider
+    $('#slider').slider({
+        animate: true,
+        min: 0,
+        max: 100,
+        value: 50
+    }).on('slidechange', function(e, ui){
+        console.log(ui.value);
+
+        var zoom = Math.max(18, ui.value);
+        var size = (30.0 * (zoom / 100.0));
+
+        $('ol.timeline li').width(size + 'em');
+        matchTimelineWidth();
+    });
+
+    // Establish basic timeline width
+    matchTimelineWidth();
 });
